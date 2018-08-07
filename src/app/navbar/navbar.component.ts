d1f686324ab6a33c362a4efa7fb68ec5a8eceb8e
Yonatan <yonatan.ramirez557@gmail.com>
2018-08-06 20:31:13 -0700

    commit

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -16,7 +16,7 @@ export class NavbarComponent implements OnInit {
 window.onscroll = function() {scrollFunction(); };
 
 function scrollFunction() {
- if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+ if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
    document.getElementById('navbar').style.top = '0';
  } else {
    document.getElementById('navbar').style.top = '-600px';
